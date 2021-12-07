09bbd65590f756ce5cd6351d9f77eaaf4a157f3d
charles-hefele <90722369+charles-hefele@users.noreply.github.com>
2021-12-06 22:29:46 -0500

    added dropdown selector for book

--- deck_explorer.py
+++ deck_explorer.py
@@ -5,11 +5,15 @@ from tkinter import ttk
 from tkinter import messagebox
 from spacy_vec_ops import SpacyVecOps
 
-# BOOK = 'Meine Sachen'
-# BOOK = 'Mein Zoo Gucklochbuch'
-# BOOK = 'Der Kleine König - Teddy ist weg'
-# BOOK = 'Kasperle auf Reisen - Chapter 1'
-BOOK = 'Kasperle auf Reisen'
+BOOKS = [
+    'Kasperle auf Reisen',
+    'Gute Nacht, kleiner Löwe!',
+    'Meine Sachen',
+    'Mein Zoo Gucklochbuch',
+    'Der Kleine König - Teddy ist weg'
+]
+
+BOOK = BOOKS[0]
 
 VOCAB_INPUT = f'vocab/{BOOK}.json'
 SHOW_LEMMAS = False
@@ -21,12 +25,27 @@ FONT_HEADER = 20
 FONT_GRID = 18
 PAD_X = 10
 NUM_SEARCH_RESULTS = 25
+INITIAL_WORD = 'Tier'
+MSG_SHOWING = 'Showing top {} related words for \'{}\':'
+
+
+def destroy_widgets(frame):
+    for widget in frame.winfo_children():
+        widget.destroy()
 
 
 class App(tk.Tk):
 
     def __init__(self):
         super().__init__()
+
+        # init widget containers
+        self.terms_text = []
+        self.terms_text_trans = []
+        self.terms_lemma = []
+        self.terms_lemma_trans = []
+        self.terms_pos = []
+        self.terms_count = []
 
         # init search term
         self.search_term = tk.StringVar()
@@ -40,9 +59,7 @@ class App(tk.Tk):
         self.style.theme_use('aqua')
 
         # init vocab
-        with open(VOCAB_INPUT, 'r') as file:
-            contents = file.read()
-            self.vocab = VocabularySchema().loads(contents)
+        self.vocab = None
 
         # configure the root window
         self.title(f'Storybooks-to-Flashcards: Deck Explorer')
@@ -50,89 +67,45 @@ class App(tk.Tk):
 
         # create the main containers
         frame_top = ttk.Frame(self)
-        frame_bottom = ttk.Frame(self)
+        frame_middle = ttk.Frame(self)
+        self.frame_bottom = ttk.Frame(self)
+
+        # configure the search bar row to have fixed width
+        frame_top.columnconfigure(0, minsize=300)
 
         # layout the main containers
         frame_top.grid(row=0, sticky='w')
-        frame_bottom.grid(row=1, sticky='w')
+        frame_middle.grid(row=1, sticky='w')
+        self.frame_bottom.grid(row=2, sticky='w')
+
+        # create and position the stats label
+        self.lbl_stats = ttk.Label(frame_top, font=('Helvetica', FONT_HEADER))
+        self.lbl_stats.grid(row=1, column=1, padx=PAD_X, sticky='w')
+
+        # create and position the 'showing' label
+        self.lbl_showing = ttk.Label(frame_middle, font=('Helvetica', FONT_HEADER))
+        self.lbl_showing.grid(row=0, column=0, padx=PAD_X, sticky='w')
+
+        # init the book variable
+        self.book = tk.StringVar(self)
+        self.book.trace_add('write', self.book_changed)
+        self.book.set(BOOKS[0])  # default value
 
         # create the widgets for the top frame
         lbl_title = ttk.Label(frame_top, text=f'Deck Explorer', font=('Helvetica', FONT_HEADER))
-        lbl_book = ttk.Label(frame_top, text=f'{BOOK} ({len(self.vocab.words)} cards)', font=('Helvetica', FONT_HEADER, 'italic'))
+        options = tk.OptionMenu(frame_top, self.book, *BOOKS)
         self.entry = ttk.Entry(frame_top, textvariable=self.search_term)
         button = ttk.Button(frame_top, text='Search', command=self.search)
-        self.lbl_showing = ttk.Label(frame_top, text=f'Showing top {NUM_SEARCH_RESULTS} related words for \'Tier\':', font=('Helvetica', FONT_HEADER))
 
         # layout the widgets for the top frame
         lbl_title.grid(row=0, column=0, padx=PAD_X, sticky='w')
-        lbl_book.grid(row=1, column=0, padx=PAD_X, sticky='w')
+        options.grid(row=1, column=0, padx=PAD_X, sticky='we')
         self.entry.grid(row=2, column=0, padx=PAD_X, sticky='we')
         button.grid(row=2, column=1, padx=PAD_X, sticky='w')
-        self.lbl_showing.grid(row=3, column=0, padx=PAD_X, sticky='w')
 
-        # init enter button bind
+        # bind the enter button
         self.entry.bind('<Return>', lambda event: self.search())
         self.entry.focus_set()
-
-        # set initial exploration word
-        exploration = self.explore(self.vocab.words, 'Tier', NUM_SEARCH_RESULTS)
-
-        # set the widget containers
-        self.terms_text = []
-        self.terms_text_trans = []
-        self.terms_lemma = []
-        self.terms_lemma_trans = []
-        self.terms_pos = []
-        self.terms_count = []
-
-        # create the header widgets for the bottom frame
-        lbl_text_header = ttk.Label(frame_bottom, text='German', font=('Helvetica bold', FONT_GRID))
-        lbl_text_trans_header = ttk.Label(frame_bottom, text='English', font=('Helvetica bold', FONT_GRID))
-        lbl_lemma_header = ttk.Label(frame_bottom, text='Lemma', font=('Helvetica bold', FONT_GRID))
-        lbl_lemma_trans_header = ttk.Label(frame_bottom, text='Lemma (English)', font=('Helvetica bold', FONT_GRID))
-        lbl_pos_header = ttk.Label(frame_bottom, text='Part of Speech', font=('Helvetica bold', FONT_GRID))
-        lbl_count_header = ttk.Label(frame_bottom, text='Occurrences', font=('Helvetica bold', FONT_GRID))
-
-        # layout the header widgets for the bottom frame
-        lbl_text_header.grid(row=0, column=1, padx=PAD_X, sticky='w')
-        lbl_text_trans_header.grid(row=0, column=2, padx=PAD_X, sticky='w')
-        if SHOW_LEMMAS:
-            lbl_lemma_header.grid(row=0, column=3, padx=PAD_X, sticky='w')
-            lbl_lemma_trans_header.grid(row=0, column=4, padx=PAD_X, sticky='w')
-        lbl_pos_header.grid(row=0, column=5, padx=PAD_X, sticky='w')
-        lbl_count_header.grid(row=0, column=6, padx=PAD_X, sticky='w')
-
-        # create the search results widgets for the bottom frame
-        for i, word in enumerate(exploration):
-            lbl_num = ttk.Label(frame_bottom, text=f'{i+1}.', font=('Helvetica', FONT_GRID))
-            lbl_text = ttk.Label(frame_bottom, text=exploration[i].text, font=('Helvetica', FONT_GRID, 'underline'),
-                                 foreground='blue', cursor='hand2')
-            lbl_text_trans = ttk.Label(frame_bottom, text=exploration[i].text_trans, font=('Helvetica', FONT_GRID))
-            lbl_lemma = ttk.Label(frame_bottom, text=exploration[i].lemma, font=('Helvetica', FONT_GRID))
-            lbl_lemma_trans = ttk.Label(frame_bottom, text=exploration[i].lemma_trans, font=('Helvetica', FONT_GRID))
-            lbl_pos = ttk.Label(frame_bottom, text=exploration[i].pos, font=('Helvetica', FONT_GRID))
-            lbl_count = ttk.Label(frame_bottom, text=exploration[i].count, font=('Helvetica', FONT_GRID))
-
-            # layout the search results widgets for the bottom frame
-            lbl_num.grid(row=i+1, column=0, padx=PAD_X, sticky='w')
-            lbl_text.grid(row=i+1, column=1, padx=PAD_X, sticky='w')
-            lbl_text_trans.grid(row=i+1, column=2, padx=PAD_X, sticky='w')
-            if SHOW_LEMMAS:
-                lbl_lemma.grid(row=i+1, column=3, padx=PAD_X, sticky='w')
-                lbl_lemma_trans.grid(row=i+1, column=4, padx=PAD_X, sticky='w')
-            lbl_pos.grid(row=i+1, column=5, padx=PAD_X, sticky='w')
-            lbl_count.grid(row=i+1, column=6, padx=PAD_X, sticky='w')
-
-            # add mouse click
-            lbl_text.bind("<Button-1>", lambda event, obj=lbl_text: self.callback(obj))
-
-            # add these labels to the label lists
-            self.terms_text.append(lbl_text)
-            self.terms_text_trans.append(lbl_text_trans)
-            self.terms_lemma.append(lbl_lemma)
-            self.terms_lemma_trans.append(lbl_lemma_trans)
-            self.terms_pos.append(lbl_pos)
-            self.terms_count.append(lbl_count)
 
     def search(self):
         term = self.search_term.get()
@@ -145,16 +118,18 @@ class App(tk.Tk):
             self.focus_force()
             self.entry.focus_set()
 
+    # when the user clicks one of the words
     def callback(self, obj):
-        term = obj['text']
-        exploration = self.explore(self.vocab.words, term, NUM_SEARCH_RESULTS)  # the word definitely exists, so no need to check for zero vector here
+        term = obj['text']  # get the text from the word label that was clicked
+        exploration = self.explore(self.vocab.words, term,
+                                   NUM_SEARCH_RESULTS)  # the word definitely exists, so no need to check for zero vector here
         self.refresh(term, exploration)
 
     def explore(self, words, query, count=10):
         return self.ops.closest_words_vocab(words, query, count)
 
     def refresh(self, term, exploration):
-        self.lbl_showing.configure(text=f'Showing top {NUM_SEARCH_RESULTS} related words for \'{term}\':')
+        self.lbl_showing.configure(text=MSG_SHOWING.format(NUM_SEARCH_RESULTS, term))
         for i, word in enumerate(exploration):
             self.terms_text[i].config(text=word.text)
             self.terms_text_trans[i].config(text=word.text_trans)
@@ -162,6 +137,84 @@ class App(tk.Tk):
             self.terms_lemma_trans[i].config(text=word.lemma_trans)
             self.terms_pos[i].config(text=word.pos)
             self.terms_count[i].config(text=word.count)
+
+    def book_changed(self, *args):
+        destroy_widgets(self.frame_bottom)
+        self.create_widgets(self.book.get())
+
+    def create_widgets(self, book):
+        # initialize the vocabulary object
+        with open(f'vocab/{book}.json', 'r') as file:
+            contents = file.read()
+            self.vocab = VocabularySchema().loads(contents)
+
+        # set the stats string
+        stats = f'{len(self.vocab.words)} cards'
+        self.lbl_stats.config(text=stats)
+
+        # set initial exploration word
+        exploration = self.explore(self.vocab.words, INITIAL_WORD, NUM_SEARCH_RESULTS)
+        self.lbl_showing.config(text=MSG_SHOWING.format(NUM_SEARCH_RESULTS, INITIAL_WORD))
+
+        # set the widget containers
+        self.terms_text = []
+        self.terms_text_trans = []
+        self.terms_lemma = []
+        self.terms_lemma_trans = []
+        self.terms_pos = []
+        self.terms_count = []
+
+        # create the header widgets for the bottom frame
+        lbl_text_header = ttk.Label(self.frame_bottom, text='German', font=('Helvetica bold', FONT_GRID))
+        lbl_text_trans_header = ttk.Label(self.frame_bottom, text='English', font=('Helvetica bold', FONT_GRID))
+        lbl_lemma_header = ttk.Label(self.frame_bottom, text='Lemma', font=('Helvetica bold', FONT_GRID))
+        lbl_lemma_trans_header = ttk.Label(self.frame_bottom, text='Lemma (English)',
+                                           font=('Helvetica bold', FONT_GRID))
+        lbl_pos_header = ttk.Label(self.frame_bottom, text='Part of Speech', font=('Helvetica bold', FONT_GRID))
+        lbl_count_header = ttk.Label(self.frame_bottom, text='Occurrences', font=('Helvetica bold', FONT_GRID))
+
+        # layout the header widgets for the bottom frame
+        lbl_text_header.grid(row=0, column=1, padx=PAD_X, sticky='w')
+        lbl_text_trans_header.grid(row=0, column=2, padx=PAD_X, sticky='w')
+        if SHOW_LEMMAS:
+            lbl_lemma_header.grid(row=0, column=3, padx=PAD_X, sticky='w')
+            lbl_lemma_trans_header.grid(row=0, column=4, padx=PAD_X, sticky='w')
+        lbl_pos_header.grid(row=0, column=5, padx=PAD_X, sticky='w')
+        lbl_count_header.grid(row=0, column=6, padx=PAD_X, sticky='w')
+
+        # create the search results widgets for the bottom frame
+        for i, word in enumerate(exploration):
+            lbl_num = ttk.Label(self.frame_bottom, text=f'{i + 1}.', font=('Helvetica', FONT_GRID))
+            lbl_text = ttk.Label(self.frame_bottom, text=exploration[i].text,
+                                 font=('Helvetica', FONT_GRID, 'underline'),
+                                 foreground='blue', cursor='hand2')
+            lbl_text_trans = ttk.Label(self.frame_bottom, text=exploration[i].text_trans, font=('Helvetica', FONT_GRID))
+            lbl_lemma = ttk.Label(self.frame_bottom, text=exploration[i].lemma, font=('Helvetica', FONT_GRID))
+            lbl_lemma_trans = ttk.Label(self.frame_bottom, text=exploration[i].lemma_trans,
+                                        font=('Helvetica', FONT_GRID))
+            lbl_pos = ttk.Label(self.frame_bottom, text=exploration[i].pos, font=('Helvetica', FONT_GRID))
+            lbl_count = ttk.Label(self.frame_bottom, text=exploration[i].count, font=('Helvetica', FONT_GRID))
+
+            # layout the search results widgets for the bottom frame
+            lbl_num.grid(row=i + 1, column=0, padx=PAD_X, sticky='w')
+            lbl_text.grid(row=i + 1, column=1, padx=PAD_X, sticky='w')
+            lbl_text_trans.grid(row=i + 1, column=2, padx=PAD_X, sticky='w')
+            if SHOW_LEMMAS:
+                lbl_lemma.grid(row=i + 1, column=3, padx=PAD_X, sticky='w')
+                lbl_lemma_trans.grid(row=i + 1, column=4, padx=PAD_X, sticky='w')
+            lbl_pos.grid(row=i + 1, column=5, padx=PAD_X, sticky='w')
+            lbl_count.grid(row=i + 1, column=6, padx=PAD_X, sticky='w')
+
+            # add mouse click
+            lbl_text.bind("<Button-1>", lambda event, obj=lbl_text: self.callback(obj))
+
+            # add these labels to the label lists
+            self.terms_text.append(lbl_text)
+            self.terms_text_trans.append(lbl_text_trans)
+            self.terms_lemma.append(lbl_lemma)
+            self.terms_lemma_trans.append(lbl_lemma_trans)
+            self.terms_pos.append(lbl_pos)
+            self.terms_count.append(lbl_count)
 
 
 if __name__ == "__main__":
